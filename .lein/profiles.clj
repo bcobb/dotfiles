0e7ef4b00e2eb3e2cf9ad0f14d73de90167d299d
Brian Cobb <bcobb@uwalumni.com>
2016-07-10 10:14:30 -0500

    Include lein and emacs config

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins
+        [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
+         [refactor-nrepl "2.2.0"]
+         [lein-cljfmt "0.5.3"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.11"]]}}
